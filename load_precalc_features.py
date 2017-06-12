0f4e0905318b90fe1b970d5e08f32c9385f958db
Andreas <mail@andreaswittmann.com>
2017-06-13 00:46:07 +0200

    impl other precalculated features

--- load_precalc_features.py
+++ load_precalc_features.py
@@ -1,8 +1,11 @@
-import scipy.io as sio
 import os
+
 import numpy as np
-from read_imgs import read_img_names
+import scipy.io as sio
+
 from file_search import get_abs_path_of_file
+from read_imgs import read_img_names
+
 
 def load_matlab_feature(path_to_imgs, feature_name):
     '''
@@ -19,18 +22,38 @@ def load_matlab_feature(path_to_imgs, feature_name):
     'D:\\PR aus Visual Computing\\Interestingness17data\\features\\'
     for img_name in img_names:
 
-        if(feature_name == 'ColorHist'):
-            feature = _colorHistogram_feature_extraction(feature_root_dir, img_name)
+        if (feature_name == 'denseSIFT_L0'):
+            feature = _denseSIFT_fe(feature_root_dir, img_name, level=0)
+        elif (feature_name == 'denseSIFT_L1'):
+            feature = _denseSIFT_fe(feature_root_dir, img_name, level=1)
+        elif (feature_name == 'denseSIFT_L2'):
+            feature = _denseSIFT_fe(feature_root_dir, img_name, level=2)
+        elif (feature_name == 'HOG_L0'):
+            feature = _hog_fe(feature_root_dir, img_name, level=0)
+        elif (feature_name == 'HOG_L1'):
+            feature = _hog_fe(feature_root_dir, img_name, level=1)
+        elif (feature_name == 'HOG_L2'):
+            feature = _hog_fe(feature_root_dir, img_name, level=2)
+        elif (feature_name == 'LBP_L0'):
+            feature = _lbp_fe(feature_root_dir, img_name, level=0)
+        elif (feature_name == 'LBP_L1'):
+            feature = _lbp_fe(feature_root_dir, img_name, level=1)
+        elif (feature_name == 'LBP_L2'):
+            feature = _lbp_fe(feature_root_dir, img_name, level=2)
+        elif (feature_name == 'GIST'):
+            feature = _gist_fe(feature_root_dir, img_name)
+        elif (feature_name == 'ColorHist'):
+            feature = _colorHist_fe(feature_root_dir, img_name)
         else:
             raise NotImplementedError
+            # TODO do we need fc7 & prob layer of AlexNet and MFCC features?
 
         features.append(feature)
-
     return np.asarray(features)
 
-def _colorHistogram_feature_extraction(feature_root_path, img_name):
-    path = os.path.join(feature_root_path, 'Features_From_FudanUniversity', 'Image_Subtask', 'ColorHist')
 
+def _colorHist_fe(feature_root_path, img_name):
+    path = os.path.join(feature_root_path, 'Features_From_FudanUniversity', 'Image_Subtask', 'ColorHist')
     version = 1
 
     matfile_path = get_abs_path_of_file(path, img_name + '.mat')
@@ -51,3 +74,68 @@ def _colorHistogram_feature_extraction(feature_root_path, img_name):
         return img_features['ColorHist'][0]
     if (version == -1):
         raise NotImplementedError
+
+
+def _denseSIFT_fe(feature_root_path, img_name, level):
+    path = os.path.join(feature_root_path, 'Features_From_FudanUniversity', 'Image_Subtask', 'denseSIFT')
+
+    matfile_path = get_abs_path_of_file(path, img_name + '.mat')
+    if (matfile_path is None):
+        return None
+    else:
+        img_features = sio.loadmat(matfile_path)
+        if level == 0:
+            return img_features['hists']['L0'][0][0]
+        elif level == 1:
+            return img_features['hists']['L1'][0][0]
+        elif level == 2:
+            return img_features['hists']['L2'][0][0]
+        else:
+            return None
+
+
+def _hog_fe(feature_root_path, img_name, level):
+    path = os.path.join(feature_root_path, 'Features_From_FudanUniversity', 'Image_Subtask', 'hog2x2')
+
+    matfile_path = get_abs_path_of_file(path, img_name + '.mat')
+    if (matfile_path is None):
+        return None
+    else:
+        img_features = sio.loadmat(matfile_path)
+        if level == 0:
+            return img_features['hists']['L0'][0][0]
+        elif level == 1:
+            return img_features['hists']['L1'][0][0]
+        elif level == 2:
+            return img_features['hists']['L2'][0][0]
+        else:
+            return None
+
+
+def _lbp_fe(feature_root_path, img_name, level):
+    path = os.path.join(feature_root_path, 'Features_From_FudanUniversity', 'Image_Subtask', 'lbp')
+
+    matfile_path = get_abs_path_of_file(path, img_name + '.mat')
+    if (matfile_path is None):
+        return None
+    else:
+        img_features = sio.loadmat(matfile_path)
+        if level == 0:
+            return img_features['hists']['L0'][0][0]
+        elif level == 1:
+            return img_features['hists']['L1'][0][0]
+        elif level == 2:
+            return img_features['hists']['L2'][0][0]
+        else:
+            return None
+
+
+def _gist_fe(feature_root_path, img_name):
+    path = os.path.join(feature_root_path, 'Features_From_FudanUniversity', 'Image_Subtask', 'gist')
+
+    matfile_path = get_abs_path_of_file(path, img_name + '.mat')
+    if (matfile_path is None):
+        return None
+    else:
+        img_features = sio.loadmat(matfile_path)
+        return img_features['descrs']
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from load_precalc_features import load_matlab_feature
 
 def main():
     do_preprocessing = False
-    calc_features = False
+    calc_features = True
     load_features = True
 
     # which features should be used
@@ -25,7 +25,20 @@ def main():
     use_rot_distance = False
     use_face_bb = False
     use_tilted_edges = False
-    use_hsv_hist = True
+    use_hsv_hist = False
+    # denseSIFT features have 3 levels of detail
+    use_denseSIFT_L0 = False
+    use_denseSIFT_L1 = False
+    use_denseSIFT_L2 = False
+    # hog features have 3 levels of detail
+    use_hog_L0 = False
+    use_hog_L1 = False
+    use_hog_L2 = False
+    # lbp features have 3 levels of detail
+    use_lbp_L0 = False
+    use_lbp_L1 = False
+    use_lbp_L2 = False
+    use_gist = True
 
     directory_root = 'D:\\PR aus Visual Computing\\Interestingness17data\\allvideos\\images'
     #directory_root = 'C:\Users\Andreas\Desktop\edge histogram problem'
@@ -71,9 +84,45 @@ def main():
             tilted_edges_uninteresting = feature_calculation(os.path.join(dir_training_data, 'uninteresting'),
                                                              img_tilted_calculator)
 
+
+        # precalculated features
         if (use_hsv_hist):
             hsv_hist_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'ColorHist')
             hsv_hist_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'ColorHist')
+
+        if use_denseSIFT_L0:
+            denseSIFT_L0_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'denseSIFT_L0')
+            denseSIFT_L0_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'denseSIFT_L0')
+        if use_denseSIFT_L1:
+            denseSIFT_L1_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'denseSIFT_L1')
+            denseSIFT_L1_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'denseSIFT_L1')
+        if use_denseSIFT_L2:
+            denseSIFT_L2_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'denseSIFT_L2')
+            denseSIFT_L2_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'denseSIFT_L2')
+
+        if use_hog_L0:
+            hog_L0_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'HOG_L0')
+            hog_L0_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'HOG_L0')
+        if use_hog_L1:
+            hog_L1_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'HOG_L1')
+            hog_L1_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'HOG_L1')
+        if use_hog_L2:
+            hog_L2_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'HOG_L2')
+            hog_L2_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'HOG_L2')
+
+        if use_lbp_L0:
+            lbp_L0_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'LBP_L0')
+            lbp_L0_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'LBP_L0')
+        if use_lbp_L1:
+            lbp_L1_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'LBP_L1')
+            lbp_L1_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'LBP_L1')
+        if use_lbp_L2:
+            lbp_L2_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'LBP_L2')
+            lbp_L2_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'LBP_L2')
+
+        if (use_gist):
+            gist_interesting = load_matlab_feature(os.path.join(dir_training_data, 'interesting'), 'GIST')
+            gist_uninteresting = load_matlab_feature(os.path.join(dir_training_data, 'uninteresting'), 'GIST')
 
 
         #
@@ -95,6 +144,41 @@ def main():
         if (use_hsv_hist):
             np.savetxt(os.path.join(dir_training_data, 'hsv_hist_interesting.gz'), hsv_hist_interesting)
             np.savetxt(os.path.join(dir_training_data, 'hsv_hist_uninteresting.gz'), hsv_hist_uninteresting)
+
+        if (use_denseSIFT_L0):
+            np.savetxt(os.path.join(dir_training_data, 'denseSIFT_L0_interesting.gz'), denseSIFT_L0_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'denseSIFT_L0_uninteresting.gz'), denseSIFT_L0_uninteresting)
+        if (use_denseSIFT_L1):
+            np.savetxt(os.path.join(dir_training_data, 'denseSIFT_L1_interesting.gz'), denseSIFT_L1_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'denseSIFT_L1_uninteresting.gz'), denseSIFT_L1_uninteresting)
+        if (use_denseSIFT_L2):
+            np.savetxt(os.path.join(dir_training_data, 'denseSIFT_L2_interesting.gz'), denseSIFT_L2_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'denseSIFT_L2_uninteresting.gz'), denseSIFT_L2_uninteresting)
+
+        if (use_hog_L0):
+            np.savetxt(os.path.join(dir_training_data, 'hog_L0_interesting.gz'), hog_L0_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'hog_L0_uninteresting.gz'), hog_L0_uninteresting)
+        if (use_hog_L1):
+            np.savetxt(os.path.join(dir_training_data, 'hog_L1_interesting.gz'), hog_L1_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'hog_L1_uninteresting.gz'), hog_L1_uninteresting)
+        if (use_hog_L2):
+            np.savetxt(os.path.join(dir_training_data, 'hog_L2_interesting.gz'), hog_L2_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'hog_L2_uninteresting.gz'), hog_L2_uninteresting)
+
+        if (use_lbp_L0):
+            np.savetxt(os.path.join(dir_training_data, 'lbp_L0_interesting.gz'), lbp_L0_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'lbp_L0_uninteresting.gz'), lbp_L0_uninteresting)
+        if (use_lbp_L1):
+            np.savetxt(os.path.join(dir_training_data, 'lbp_L1_interesting.gz'), lbp_L1_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'lbp_L1_uninteresting.gz'), lbp_L1_uninteresting)
+        if (use_lbp_L2):
+            np.savetxt(os.path.join(dir_training_data, 'lbp_L2_interesting.gz'), lbp_L2_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'lbp_L2_uninteresting.gz'), lbp_L2_uninteresting)
+
+        if (use_gist):
+            np.savetxt(os.path.join(dir_training_data, 'gist_interesting.gz'), gist_interesting)
+            np.savetxt(os.path.join(dir_training_data, 'gist_uninteresting.gz'), gist_uninteresting)
+
 
         print 'feature calculation finished.'
 
@@ -132,10 +216,43 @@ def main():
             #plt.title('Hist tilted_edges_uninteresting')
             #plt.show()
 
-        if (use_hsv_hist):
+        if use_hsv_hist:
             hsv_hist_interesting = np.loadtxt(os.path.join(dir_training_data, 'hsv_hist_interesting.gz'))
             hsv_hist_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'hsv_hist_uninteresting.gz'))
 
+        if use_denseSIFT_L0:
+            denseSIFT_L0_interesting = np.loadtxt(os.path.join(dir_training_data, 'denseSIFT_L0_interesting.gz'))
+            denseSIFT_L0_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'denseSIFT_L0_uninteresting.gz'))
+        if use_denseSIFT_L1:
+            denseSIFT_L1_interesting = np.loadtxt(os.path.join(dir_training_data, 'denseSIFT_L1_interesting.gz'))
+            denseSIFT_L1_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'denseSIFT_L1_uninteresting.gz'))
+        if use_denseSIFT_L2:
+            denseSIFT_L2_interesting = np.loadtxt(os.path.join(dir_training_data, 'denseSIFT_L2_interesting.gz'))
+            denseSIFT_L2_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'denseSIFT_L2_uninteresting.gz'))
+
+        if use_hog_L0:
+            hog_L0_interesting = np.loadtxt(os.path.join(dir_training_data, 'hog_L0_interesting.gz'))
+            hog_L0_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'hog_L0_uninteresting.gz'))
+        if use_hog_L1:
+            hog_L1_interesting = np.loadtxt(os.path.join(dir_training_data, 'hog_L1_interesting.gz'))
+            hog_L1_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'hog_L1_uninteresting.gz'))
+        if use_hog_L2:
+            hog_L2_interesting = np.loadtxt(os.path.join(dir_training_data, 'hog_L2_interesting.gz'))
+            hog_L2_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'hog_L2_uninteresting.gz'))
+
+        if use_lbp_L0:
+            lbp_L0_interesting = np.loadtxt(os.path.join(dir_training_data, 'lbp_L0_interesting.gz'))
+            lbp_L0_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'lbp_L0_uninteresting.gz'))
+        if use_lbp_L1:
+            lbp_L1_interesting = np.loadtxt(os.path.join(dir_training_data, 'lbp_L1_interesting.gz'))
+            lbp_L1_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'lbp_L1_uninteresting.gz'))
+        if use_lbp_L2:
+            lbp_L2_interesting = np.loadtxt(os.path.join(dir_training_data, 'lbp_L2_interesting.gz'))
+            lbp_L2_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'lbp_L2_uninteresting.gz'))
+
+        if use_gist:
+            gist_interesting = np.loadtxt(os.path.join(dir_training_data, 'gist_interesting.gz'))
+            gist_uninteresting = np.loadtxt(os.path.join(dir_training_data, 'gist_uninteresting.gz'))
 
 
     #
@@ -161,6 +278,40 @@ def main():
         hsv_hist_interesting = preprocessing.scale(hsv_hist_interesting)
         hsv_hist_uninteresting = preprocessing.scale(hsv_hist_uninteresting)
 
+    if use_denseSIFT_L0:
+        denseSIFT_L0_interesting = preprocessing.scale(denseSIFT_L0_interesting)
+        denseSIFT_L0_uninteresting = preprocessing.scale(denseSIFT_L0_uninteresting)
+    if use_denseSIFT_L1:
+        denseSIFT_L1_interesting = preprocessing.scale(denseSIFT_L1_interesting)
+        denseSIFT_L1_uninteresting = preprocessing.scale(denseSIFT_L1_uninteresting)
+    if use_denseSIFT_L2:
+        denseSIFT_L2_interesting = preprocessing.scale(denseSIFT_L2_interesting)
+        denseSIFT_L2_uninteresting = preprocessing.scale(denseSIFT_L2_uninteresting)
+
+    if use_hog_L0:
+        hog_L0_interesting = preprocessing.scale(hog_L0_interesting)
+        hog_L0_uninteresting = preprocessing.scale(hog_L0_uninteresting)
+    if use_hog_L1:
+        lbp_L1_interesting = preprocessing.scale(hog_L1_interesting)
+        hog_L1_uninteresting = preprocessing.scale(hog_L1_uninteresting)
+    if use_hog_L2:
+        hog_L2_interesting = preprocessing.scale(hog_L2_interesting)
+        hog_L2_uninteresting = preprocessing.scale(hog_L2_uninteresting)
+
+    if use_lbp_L0:
+        lbp_L0_interesting = preprocessing.scale(lbp_L0_interesting)
+        lbp_L0_uninteresting = preprocessing.scale(lbp_L0_uninteresting)
+    if use_lbp_L1:
+        lbp_L1_interesting = preprocessing.scale(lbp_L1_interesting)
+        lbp_L1_uninteresting = preprocessing.scale(lbp_L1_uninteresting)
+    if use_lbp_L2:
+        lbp_L2_interesting = preprocessing.scale(lbp_L2_interesting)
+        lbp_L2_uninteresting = preprocessing.scale(lbp_L2_uninteresting)
+
+    if (use_gist):
+        gist_interesting = preprocessing.scale(gist_interesting)
+        gist_uninteresting = preprocessing.scale(gist_uninteresting)
+
     #
     # concatenate features
     #
@@ -182,6 +333,30 @@ def main():
 
     if (use_hsv_hist):
         hsv_hists = np.concatenate((hsv_hist_interesting, hsv_hist_uninteresting), axis=0)
+
+    if use_denseSIFT_L0:
+        denseSIFT_L0 = np.concatenate((denseSIFT_L0_interesting, denseSIFT_L0_uninteresting), axis=0)
+    if use_denseSIFT_L1:
+        denseSIFT_L1 = np.concatenate((denseSIFT_L1_interesting, denseSIFT_L1_uninteresting), axis=0)
+    if use_denseSIFT_L2:
+        denseSIFT_L2 = np.concatenate((denseSIFT_L2_interesting, denseSIFT_L2_uninteresting), axis=0)
+
+    if use_hog_L0:
+        hog_L0 = np.concatenate((hog_L0_interesting, hog_L0_uninteresting), axis=0)
+    if use_hog_L1:
+        hog_L1 = np.concatenate((hog_L1_interesting, hog_L1_uninteresting), axis=0)
+    if use_hog_L2:
+        hog_L2 = np.concatenate((hog_L2_interesting, hog_L2_uninteresting), axis=0)
+
+    if use_lbp_L0:
+        lbp_L0 = np.concatenate((lbp_L0_interesting, lbp_L0_uninteresting), axis=0)
+    if use_lbp_L1:
+        lbp_L1 = np.concatenate((lbp_L1_interesting, lbp_L1_uninteresting), axis=0)
+    if use_lbp_L2:
+        lbp_L2 = np.concatenate((lbp_L2_interesting, lbp_L2_uninteresting), axis=0)
+
+    if (use_gist):
+        gists = np.concatenate((gist_interesting, gist_uninteresting), axis=0)
 
     #
     # reshape 1D arrays to 2D arrays
@@ -225,6 +400,63 @@ def main():
             X = np.c_[X, hsv_hists]
         except NameError:
             X = hsv_hists
+    if use_denseSIFT_L0:
+        try:
+            X = np.c_[X, denseSIFT_L0]
+        except NameError:
+            X = denseSIFT_L0
+    if use_denseSIFT_L1:
+        try:
+            X = np.c_[X, denseSIFT_L1]
+        except NameError:
+            X = denseSIFT_L1
+    if use_denseSIFT_L2:
+        try:
+            X = np.c_[X, denseSIFT_L2]
+        except NameError:
+            X = denseSIFT_L2
+
+    if use_hog_L0:
+        try:
+            X = np.c_[X, hog_L0]
+        except NameError:
+            X = hog_L0
+    if use_hog_L1:
+        try:
+            X = np.c_[X, hog_L1]
+        except NameError:
+            X = hog_L1
+    if use_hog_L2:
+        try:
+            X = np.c_[X, hog_L2]
+        except NameError:
+            X = hog_L2
+
+    if use_lbp_L0:
+        try:
+            X = np.c_[X, lbp_L0]
+        except NameError:
+            X = lbp_L0
+    if use_lbp_L1:
+        try:
+            X = np.c_[X, lbp_L1]
+        except NameError:
+            X = lbp_L1
+    if use_lbp_L2:
+        try:
+            X = np.c_[X, lbp_L2]
+        except NameError:
+            X = lbp_L2
+    if (use_gist):
+        try:
+            X = np.c_[X, gists]
+        except NameError:
+            X = gists
+
+
+
+
+    #TODO do pca analysis
 
     #
     # get interestingness
