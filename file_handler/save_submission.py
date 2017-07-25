7b78e151d379a88d039ccc431074991911db232f
Andreas <mail@andreaswittmann.com>
2017-07-25 11:43:44 +0200

    rename

--- file_handler/save_submission.py
+++ file_handler/save_submission.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def save_submission(submission_format, runname):
-    np.savetxt('me17in_groupname_image_{}_2nd.txt'.format(runname), submission_format, delimiter=',', fmt='%s')
+    np.savetxt('me17in_groupname_image_{}_.txt'.format(runname), submission_format, delimiter=',', fmt='%s')
